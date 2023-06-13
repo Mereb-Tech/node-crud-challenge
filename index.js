135e83752e900f36892e2f3de83ea94376b61890
Yagth <yabuman46@gmail.com>
2023-06-13 14:48:07 +0300

    Created the GET for person with a specific id

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ let persons = [
     age: "26",
     hobbies: [],
   },
+  {
+    id: "2",
+    name: "Nat",
+    age: "22",
+    hobbies: [],
+  },
+  {
+    id: "3",
+    name: "Dan",
+    age: "30",
+    hobbies: [],
+  },
 ]; //This is your in memory database
 
 app.set("db", persons);
 //TODO: Implement crud of person
 
-app.get("/person", (req, res) => {
+app.get("/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/persons/:personId", (req, res) => {
+  const personId = req.params.personId;
+  res.json(persons[personId - 1]);
+});
+
 if (require.main === module) {
+  console.log("Server up and running on port 3000...");
   app.listen(3000);
 }
 module.exports = app;
